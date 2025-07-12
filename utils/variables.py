e468f0f21f21f03bdc68e3a0cfebb35557f3d3bb
Ederson Cruz <edersoncruz1337@gmail.com>
2025-07-12 17:16:47 -0300

    Update

--- utils/variables.py
+++ utils/variables.py
@@ -5,4 +5,4 @@ ROOT_DIR = Path(__file__).parent
 FILES_DIR = ROOT_DIR / 'files'
 WINDOW_ICON_PATH = FILES_DIR / 'logo.png'
 BASE_DIR = BASE_DIR = Path(__file__).resolve().parent.parent
-HABITS_FILE = os.path.join(BASE_DIR, "files/habbits.json")
+HABITS_FILE = os.path.join(BASE_DIR, "files/habits.json")
